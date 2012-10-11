fa423f479ae71084a07cb5ff3ca3c1635e04c307
Ivan Teoh <ivan@pretaweb.com>
2012-10-11 11:41:32 +1100

    Code clean up.

--- transmogrify/siteanalyser/relinker.py
+++ transmogrify/siteanalyser/relinker.py
@@ -1,7 +1,6 @@
-
 from zope.interface import implements
 from zope.interface import classProvides
-from zope.component import queryUtility
+#from zope.component import queryUtility
 
 from collective.transmogrifier.interfaces import ISectionBlueprint
 from collective.transmogrifier.interfaces import ISection
@@ -9,14 +8,14 @@ import urllib
 from lxml import etree
 import lxml
 from urlparse import urljoin
-import urllib
+#import urllib
 from external.relative_url import relative_url
-from sys import stderr
-from collective.transmogrifier.utils import Expression
+#from sys import stderr
+#from collective.transmogrifier.utils import Expression
 import logging
-from external.normalize import urlnormalizer as normalizer
+#from external.normalize import urlnormalizer as normalizer
 import urlparse
-from sys import stderr
+#from sys import stderr
 #from plone.i18n.normalizer import urlnormalizer as normalizer
 
 
@@ -27,49 +26,45 @@ class Counter:
 class Relinker(object):
     classProvides(ISectionBlueprint)
     implements(ISection)
-    
+
     def __init__(self, transmogrifier, name, options, previous):
         self.previous = previous
         self.name = name
         self.logger = logging.getLogger(name)
 
     def __iter__(self):
-        
         #TODO need to fix relative links
-        
 
         changes = {}
         bad = {}
         items = {}
-        
+
         self.missing = set([])
         self.bad_pages = 0
-        
+
         for item in self.previous:
-            path = item.get('_path',None)
+            path = item.get('_path', None)
             if path is None:
                 url = item.get('_bad_url')
                 if url:
                     bad[url] = item
                 yield item
                 continue
-            base = item.get('_site_url','')
+            base = item.get('_site_url', '')
 
             origin = item.get('_origin')
             if not origin:
                 origin = item['_origin'] = path
             else:
                 self.logger.debug("%s <- %s (relinked)" % (path, origin))
-            link = urllib.unquote_plus(base+origin)
+            link = urllib.unquote_plus(base + origin)
 
             if link in changes:
                 raise Exception("duplicate redirects: both '%s 'and '%s' were redirected from '%s'" %
                             (changes[link]['_path'], item['_path'], link))
 
-
             changes[link] = item
             items[path] = item
-
 
         for item in changes.values():
             counter = Counter()
@@ -95,8 +90,8 @@ class Relinker(object):
                         self.logger.debug("'%s' default page stay" % (item['_path']))
                     else:
                         #import pdb; pdb.set_trace()
-                        self.logger.warning("'%s' default page '%s' was moved out of this folder (%s)"%
-                                            (item['_path'],item['_defaultpage'], newindex['_path']))
+                        self.logger.warning("'%s' default page '%s' was moved out of this folder (%s)" %
+                                            (item['_path'], item['_defaultpage'], newindex['_path']))
                         del item['_defaultpage']
                         # leave in defaultpage and hope redirection takes care of it
                     #    import pdb; pdb.set_trace()
@@ -112,36 +107,35 @@ class Relinker(object):
                         self.logger.warning("'%s' had default page '%s' set but it is missing. Removing defaultpage reference" % (item['_path'], item['_defaultpage']))
                         del item['_defaultpage']
             if 'remoteUrl' in item:
-                link = item['_site_url']+urljoin(item['_origin'],item['remoteUrl'])
+                link = item['_site_url'] + urljoin(item['_origin'], item['remoteUrl'])
                 # have to put ./ in front of Link
-                item['remoteUrl'] = "./"+replace(link, item, changes, counter, self.missing, bad)
+                item['remoteUrl'] = "./" + replace(link, item, changes, counter, self.missing, bad)
 
             if item.get('_mimetype') in ['text/xhtml', 'text/html']:
                 self.relinkHTML(item, changes, counter, bad)
 
             del item['_origin']
             #rewrite the backlinks too
-            backlinks = item.get('_backlinks',[])
+            backlinks = item.get('_backlinks', [])
             newbacklinks = []
-            for origin,name in backlinks:
+            for origin, name in backlinks:
                 #assume absolute urls
-                backlinked= changes.get(origin)
+                backlinked = changes.get(origin)
                 if backlinked:
-                    backlink = backlinked['_site_url']+backlinked['_path']
-                    newbacklinks.append((backlink,name))
+                    backlink = backlinked['_site_url'] + backlinked['_path']
+                    newbacklinks.append((backlink, name))
                 else:
-                    newbacklinks.append((origin,name))
+                    newbacklinks.append((origin, name))
             if backlinks:
                 item['_backlinks'] = newbacklinks
 
             yield item
         if self.missing:
             self.logger.warning("%d broken internal links in %d pages. "
-                                "Content maybe missing. Debug to see details." % (len(self.missing),self.bad_pages))
-
+                                "Content maybe missing. Debug to see details." % (len(self.missing), self.bad_pages))
 
     def relinkHTML(self, item, changes, counter, bad={}):
-        oldbase = item['_site_url']+item['_origin']
+        oldbase = item['_site_url'] + item['_origin']
         # guess which fields are html by trying to parse them
         html = {}
         for key, value in item.items():
@@ -182,6 +176,7 @@ class Relinker(object):
      #       msg = "ERROR: relinker parse error %s, %s" % (path,str(Exception))
      #       logger.log(logging.ERROR, msg, exc_info=True)
 
+
 def swapfragment(link, newfragment):
     t = urlparse.urlparse(link)
     fragment = t[-1]
@@ -189,9 +184,10 @@ def swapfragment(link, newfragment):
     link = urlparse.urlunparse(t)
     return link, fragment
 
+
 def replace(link, item, changes, counter, missing, bad):
     path = item['_path']
-    newbase = item['_site_url']+path
+    newbase = item['_site_url'] + path
 
     link, fragment = swapfragment(link, '')
 
@@ -200,7 +196,7 @@ def replace(link, item, changes, counter, missing, bad):
         linked = changes.get(urllib.unquote_plus(link))
 
     if linked:
-        linkedurl = item['_site_url']+linked['_path']
+        linkedurl = item['_site_url'] + linked['_path']
         newlink = swapfragment(relative_url(newbase, linkedurl), fragment)[0]
         counter.counter += 1
     else:
